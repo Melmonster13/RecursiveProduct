d8df469730448c198336103580f3db14daf1c235
Mel <melly.cadd@gmail.com>
2025-01-26 21:56:18 -0700

    Initial Commit
    Shared to GitHub

--- src/main/java/RecursiveProduct.java
+++ src/main/java/RecursiveProduct.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class RecursiveProduct {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Please enter number: " + (i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        System.out.println("Product: " + calculateProduct(numbers, 0));
+        input.close();
+    }
+
+    public static int calculateProduct(int[] numbers, int index) {
+        if (index >= numbers.length) {
+            return 1;
+        }
+        return numbers[index] * calculateProduct(numbers, index + 1);
+    }
 }
